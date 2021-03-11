e75141369d5b763e0aca754a2d1b7d2341566e94
RafaelRResnauer <resnauer.rafael@gmail.com>
2021-03-11 14:56:58 -0300

    Initial commit

---
+++ src/main/java/Monster.java
@@ -0,0 +1,9 @@
+public class Monster {
+    private String name;
+    private String type;
+    private int armorClass;
+    private String armorType;
+    private int hitPoints;
+    private String hpFormula;
+    private String speed;
+}
